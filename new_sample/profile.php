895b7ea26c83e521447744effcd5d6c5f2423336
Ashton <ashtonrbay@gmail.com>
2020-09-08 01:21:09 +0200

    completed styling profile page

--- new_sample/profile.php
+++ new_sample/profile.php
@@ -58,6 +58,7 @@
 
         <div class="wrapper">
 
+            <!-- personal info -->
             <div class="personal-info tile profile-first">
                 <div class="profile-pic">
                     <img src="./images/lion.png" alt="profile-pic">
@@ -72,8 +73,115 @@
 
             </div>
 
-        </div>
+             <!-- bio section and spirit animal -->
+            <div class="bio-and-spirit-animal tile profile-first">
 
+                <p class="bio">
+                    <span class="bio-heading">Bio:</span>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias tempora exercitationem aliquid nulla iure illo itaque dolorem eveniet voluptatibus! Dicta ducimus quaerat exercitationem quae distinctio hic, cupiditate assumenda praesentium eum aut officia qui beatae cum porro rem quam eius! Exercitationem dignissimos asperiores aut ipsum ullam repudiandae neque! Illo exercitationem fuga numquam dignissimos. Sit aperiam inventore esse magni possimus similique voluptas fugit non reiciendis dolorum modi minima cumque provident, dolores ducimus quibusdam sunt accusantium unde quisquam labore! Aperiam nobis vitae recusandae numquam sint perferendis aspernatur, quidem veniam eligendi et quo qui dolores autem. Veritatis corporis labore accusantium voluptatum omnis, ad maxime.
+                </p>
+
+                <p class="spirit-animal">
+                    <span class="bio-heading">Spirit animal:</span>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, debitis quam dignissimos iusto, cupiditate voluptas vero laboriosam eos qui dolores a voluptatum enim, quaerat ipsam odio non quod harum. Quis recusandae, autem soluta error magnam omnis excepturi accusantium suscipit quos dolor, atque neque a! Quas aspernatur velit unde illo praesentium minus consequatur dolore doloribus quaerat magnam, voluptatibus fugiat rerum placeat quam possimus. Velit amet pariatur qui optio hic iste labore. Unde minima excepturi blanditiis. Nisi omnis repellat dolores qui est?
+                </p>
+
+            </div>
+
+
+            <!-- hobbies and interests -->
+            <div class="hobbies-and-interests tile profile-first">
+                    <h3>Hobbies:</h3>
+                <div class="hobbies">
+
+                    <div class="hobbie-tile tile"> 
+                            <img src="./images/hobbies/coding.png" alt="hobbie-1" class="hobbie-1">
+                    </div>
+
+                    <div class="hobbie-tile tile">
+                        <img src="./images/hobbies/gym.png" alt="hobbie-2" class="hobbie-2">
+                    </div>
+                    
+                    <div class="hobbie-tile tile">
+                        <img src="./images/hobbies/movies.png" alt="hobbie-3" class="hobbie-2">
+                    </div>
+
+                    <div class="hobbie-tile tile">
+                        <img src="./images/hobbies/music.png" alt="hobbie-4" class="hobbie-2">
+                    </div>     
+                
+
+                </div>
+
+            </div>
+
+
+            <!-- bucket list -->
+            <div class="bucket-list-and-goals tile profile-first">
+
+                <h4>Bucket List:</h4>
+                
+                    <div class="bucket-list">
+
+                        <ul>
+                            <li><span>1. </span>Visit Iceland in winter.</li>
+                            <li><span>2. </span>Build something that helps people.</li>
+                            <li><span>3. </span>Become fluent in a foreign language.</li>
+                            <li><span>4. </span>Have lunch with Elon Musk.</li>
+                        </ul>
+
+                    </div>
+
+                 <h4>Goals:</h4>   
+
+                    <div class="goals-and-aspirations">
+
+                        <ul>
+                            <li><span>1. </span>Visit Iceland in winter.</li>
+                            <li><span>2. </span>Build something that helps people.</li>
+                            <li><span>3. </span>Become fluent in a foreign language.</li>
+                            <li><span>4. </span>Have lunch with Elon Musk.</li>
+                        </ul>
+
+                    </div>
+                
+
+            </div>
+
+
+            <div class="friends tile profile-first">
+
+                <h3>Friends:</h3>
+
+                <a href="#" class="friend-mini-profile tile">
+                    <img src="./images/penguin.png" alt="profile-pic">
+                    <p>Zoey Deutch</p>
+                </a>
+
+                <a href="#" class="friend-mini-profile tile">
+                    <img src="./images/bear.png" alt="profile-pic">
+                    <p>John Green</p>
+                </a>
+
+                <a href="#" class="friend-mini-profile tile">
+                    <img src="./images/fox.png" alt="profile-pic">
+                    <p>Christian Bale</p>
+                </a>
+
+                <a href="#" class="friend-mini-profile tile">
+                    <img src="./images/tiger.png" alt="profile-pic">
+                    <p>Christopher Nolan</p>
+                </a>
+
+                <a href="#">See more...</a>
+            </div>
+
+
+            <!-- end wrapper -->
+        </div>       
+
+
+    <!-- end sub body -->
     </div>
 
     <div class="sidebar">
